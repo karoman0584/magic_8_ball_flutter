6f5607d2c52fb67f03f106f2a3f4da7931e616af
Kader <karoman0584@gmail.com>
2021-03-25 09:21:31 -0300

    Project done

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Magic 8 ball flutter
 Magic 8 Ball flutter project
+
+
+## What you will create
+
+![Finished App](https://github.com/karoman0584/Images/blob/main/magic-8-ball-flutter.gif)
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,47 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: BallPage(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      backgroundColor: Colors.blue,
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Expanded(
+        child: FlatButton(
+          onPressed: () {
+            setState(() {
+              ballNumber = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image.asset('images/ball$ballNumber.png'),
+        ),
+      ),
+    );
   }
 }
